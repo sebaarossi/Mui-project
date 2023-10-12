3c8f901ec2884483745d79e5b20fc8399ea205fa
sebastian.rossi <sebastiannrossi@gmail.com>
2023-10-12 15:43:42 -0300

    data table

--- src/Components/Table/ContactTable.jsx
+++ src/Components/Table/ContactTable.jsx
@@ -1,6 +1,41 @@
+import { Table, TableContainer, TableCell, TableBody, TableHead, TableRow, Tab} from "@mui/material"
+import { contactData } from "../../Data/ContactData"
+
 
 export default function ContactTable() {
     return (
-       <div>Table</div> 
+       <TableContainer>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Role</TableCell>
+                        <TableCell>Skills</TableCell>
+                        <TableCell>Start Date</TableCell>
+                        <TableCell>Preference</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {
+                        contactData.map((contact) => {
+                            return (
+                                <TableRow key={contact.name}>
+                                    {
+                                        Object.entries(contact).map(([key, value]) =>{
+                                            if(key !== "id"){
+                                            return (
+                                                <TableCell>{value}</TableCell>
+                                            )}
+                                            return ""
+                                        })
+                                    }
+                                </TableRow>
+                            )
+                        })
+                    }
+
+                </TableBody>
+            </Table>
+       </TableContainer>
     )
 }
