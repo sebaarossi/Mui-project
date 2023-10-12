cd3d20c96493fae109d50de0b58fb5c621512618
sebastian.rossi <sebastiannrossi@gmail.com>
2023-10-12 15:20:17 -0300

    data grid

--- src/Components/Grid/ContactCardGrid.jsx
+++ src/Components/Grid/ContactCardGrid.jsx
@@ -1,6 +1,53 @@
+import {Avatar, Card, CardContent, CardHeader, Grid, List, ListSubheader, Typography} from "@mui/material"
+import { contactData } from "../../Data/ContactData"
+
 
 export default function ContactDataGrid() {
     return (
-       <div>Data Grid</div> 
+       <Grid container spacing={2} sx={{width: 700}}>
+            {
+                contactData.map((contact) => {
+                    return (
+                        <Grid Item key={contact.name}>
+                            <Card sx={{width: 300}}>
+                                <CardHeader
+                                    title={contact.name}
+                                    subheader={contact.role}
+                                    avatar={
+                                        <Avatar>{contact.name?.substring(0,1).toUpperCase() || "A"}</Avatar>
+                                    }
+                                />
+                                <CardContent>
+                                    <Typography>
+                                        <b>Start Date: </b> {contact.startDate}
+                                    </Typography>
+                                    <Typography>
+                                        <b>Preference: </b> {contact.preference}
+                                    </Typography>
+                                    <List
+                                        sx={{ listStyle: "list-item", listStyleType: "circle", padding: 2}}
+                                        subheader={
+                                            <ListSubheader
+                                                sx={{rigth: 16, position: "inherit", fontSize: "1.25rem", color: "black", paddingLeft: 0}}
+                                            >
+                                                Skills:
+                                            </ListSubheader>
+                                        }
+                                    >
+                                        {contact.skills.map((skill) => {
+                                            return(
+                                                <li style={{paddingBottom: 2}}>{skill}</li>
+                                            )
+                                        })}
+                                        
+                                    </List>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    )
+                })
+            }
+
+       </Grid>
     )
 }
